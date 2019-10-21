20130c5fccd972e0ee6c15805369dbdcabdd4957
Iván Moreno <contacto@ivanmoreno.me>
2019-10-21 13:38:46 +0200

    [improvement] CRUD operations support. Access to firestore database

--- functions/src/lib/languages/controller.js
+++ functions/src/lib/languages/controller.js
@@ -1,27 +1,61 @@
-const languages = require('./languages.json');
+const admin = require("firebase-admin");
+const serviceAccount = require(process.env.PATH_TO_SERVICEACCOUNTKEY);
 
-function getAllLanguages(_req, res) {
-    return res.json({
-       success: true,
-       data: languages,
-    });
+admin.initializeApp({ credential: admin.credential.cert(serviceAccount) });
+
+const db = admin.firestore();
+
+const languagesCollection = 'languages';
+
+async function getAllLanguages(_req, res) {
+    try {
+        const languages = await db.collection(languagesCollection).get();
+        const data = languages.docs.map(doc => doc.data());
+        return res.json({
+           success: true,
+           data,
+        });
+    } catch(error) {
+        res.send('Internal server error', 500);
+    }
 }
 
-function getLanguage({ params: { language } }, res) {
-    const lang = languages.filter(lang => lang.name === language);
-    const exists = lang.length > 0;
-
-    return res
-        .status(exists ? 200 : 404)
-        .json({
-            success: exists,
-            data: exists
-                ? lang[0]
-                : 'Language not found',
+async function getLanguage({ params: { language } }, res) {
+    try {
+        const languageRef = await db.collection(languagesCollection).where('name', '==', language).get();
+        const data = languageRef.docs[0].data();
+        return res.json({
+            success: true,
+            data,
         });
+    } catch(error) {
+        res.send('Language not found', 400);
+    }
+}
+
+async function addLanguage({ body: { name } }, res) {
+    try {
+        const docRef = await db.collection(languagesCollection).add({ name });
+        const document = await docRef.get();
+        res.send(document.data());
+    } catch(error) {
+        res.send('Internal server error', 500);
+    }
+}
+
+async function deleteLanguage({ params: { language } }, res) {
+    try {
+        const languageRef = await db.collection(languagesCollection).where('name', '==', language).get();
+        languageRef.docs.forEach(({ ref }) => ref.delete());
+        res.send(200);
+    } catch(error) {
+        res.send('Language not found', 400);
+    }
 }
 
 module.exports = {
     getAllLanguages,
     getLanguage,
+    addLanguage,
+    deleteLanguage
 };
--- functions/src/lib/languages/route.js
+++ functions/src/lib/languages/route.js
@@ -1,7 +1,9 @@
 const router = require('express').Router();
 const controller = require('./controller');
 
+router.post('/', controller.addLanguage);
 router.get('/all', controller.getAllLanguages);
 router.get('/:language', controller.getLanguage);
+router.delete('/:language', controller.deleteLanguage);
 
 module.exports = router;
